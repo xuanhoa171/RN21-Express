ec44c71409411882b62a4461aea00c83009d662b
Xuan Hoa <xuanhoa17011@gmail.com>
2023-07-03 13:26:10 +0700

    import thu vien node fecth, tao enpoint, get call api

--- app-2.js
+++ app-2.js
@@ -1,41 +1,69 @@
-
-let express = require('express');
-let logger = require("morgan")
-let fs = require("fs")
+let express = require("express");
+let logger = require("morgan");
+let fs = require("fs");
 let app = express();
-
+let commentData = require("./data/comment.json")
+let fetch =require("node-fecth").default
 
 // app.use();  middware
-// app.use("enpoint", callback);  1 enpoint 1 callback 
+// app.use("enpoint", callback);  1 enpoint 1 callback
 
 let home_handler = (req, res) => {
   res.json({
-    "message": "Hello from application"
-  })
-}
+    message: "Hello from application",
+  });
+};
 
 let user_handler = (req, res, next) => {
-   fs.readFile('test.txt', 'utf8', (err, data) => {
-      if (err) {
-        next(err)
-        return;
-      }
-     res.json(data);
-    });
-}
+  fs.readFile("data/users.json", "utf8", (err, data) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(data);
+  });
+};
+
+//  read data từ file comments -> trả về cho người dùng
+// let comment_handler = (req, res, next) => {
+//    fs.readFile("data/comment.json", "utf8", (err, data) => {
+//      if (err) {
+//        next(err);
+//        return;
+//      }
+//    //   res.json(data);
+//     JSON.parse(res.data)
+//    });
+//  };
+
+
 
 let error_handler = (err, req, res) => {
-   res.status(500).json({
-      message:err.message
-   })
-}
+  res.status(500).json({
+    message: err.message,
+  });
+};
 
-app.use(logger("dev"))
 
-// regex string 
-app.use("/user", user_handler)
-app.use("/", home_handler)
-app.use(error_handler)
+app.get("comment", (req, res) => {
+   res.send(commentData)
+})
+
+app.get("/pokemon", (req,res)=> {
+   const dataPokemon = await(
+      await.fetch(" https://pokeapi.co/api/v2/pokemon/ditto")
+   ).json();
+   res.send(dataPokemon)
+})
+
+
+app.use(logger("dev"));
+
+// regex string
+app.use("/pokemon")
+// app.use("/comment", comment_handler);
+app.use("/user", user_handler);
+app.use("/", home_handler);
+app.use(error_handler);
 
 module.exports = app;
-
